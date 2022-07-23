691e5a61a09c0214ffdf8ef2318b63e6e355f36a
t-bello7 <bello4aus@gmail.com>
2022-07-23 12:07:56 +0100

    add style to add book form

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -49,21 +49,21 @@ const AddBook = () => {
         <form onSubmit={handleSubmit} className="container add__container">
             <h3> Add New Book    </h3>
             <div className="flex">
-            <label htmlFor="title">
-                <input type="text" placeholder="Book Title" name="title" value={formInput.title} onChange={handleChange}/>
-            </label>
-            <label htmlFor="author">
-                <input type="text" placeholder="Book Author" name="author" value={formInput.author} onChange={handleChange}/>
-            </label>
-            <label htmlFor="category">
-                <select name="category" value={formInput.category} onChange={handleChange}>
-                    <option value=""></option>
-                    <option key="economy" value="economy">Economy</option>
-                    <option key="action" value="action">Action</option>
-                    <option key="science-fiction" value="science-fiction">Science Fiction</option>
-                </select>
-            </label>
-            <button type="submit">Add Book</button>
+                <label htmlFor="title">
+                    <input type="text" placeholder="Book Title" name="title" value={formInput.title} onChange={handleChange} className="add__input title"/>
+                </label>
+                <label htmlFor="author">
+                    <input type="text" placeholder="Book Author" name="author" value={formInput.author} onChange={handleChange} className="add__input"/>
+                </label>
+                <label htmlFor="category">
+                    <select name="category" value={formInput.category} onChange={handleChange} className="add__input">
+                        <option value=""></option>
+                        <option key="economy" value="economy">Economy</option>
+                        <option key="action" value="action">Action</option>
+                        <option key="science-fiction" value="science-fiction">Science Fiction</option>
+                    </select>
+                </label>
+                <button className="btn" type="submit">Add Book</button>
             </div>
         </form>
     )
